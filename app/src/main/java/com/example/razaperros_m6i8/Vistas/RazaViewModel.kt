4357ca5f2e20a8013106e1b8f95d3842ac1a6ed5
EnzoGatica <enzogatica96@gmail.com>
2023-08-07 22:20:40 -0400

    se agrega VM

--- app/src/main/java/com/example/razaperros_m6i8/Vistas/RazaViewModel.kt
+++ app/src/main/java/com/example/razaperros_m6i8/Vistas/RazaViewModel.kt
@@ -1,4 +1,26 @@
 package com.example.razaperros_m6i8.Vistas
 
-class RazaViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.razaperros_m6i8.data.Repositorio
+import com.example.razaperros_m6i8.data.local.RazaDatabase
+import com.example.razaperros_m6i8.data.remote.PerrosRetrofit
+import kotlinx.coroutines.launch
+
+class RazaViewModel(application: Application): AndroidViewModel(application) {
+
+    private val repositorio: Repositorio
+
+    init {
+        val api = PerrosRetrofit.gesRetroFitAves()
+        val razaDatabase = RazaDatabase.getDatabase(application).getRazaDao()
+        repositorio = Repositorio(api,razaDatabase)
+    }
+
+    fun getAllRazas() = viewModelScope.launch {
+        repositorio.getRazas()
+    }
+
+
 }
